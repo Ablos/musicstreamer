1bb275ca5a9b08fcacc5516baa2a2528143dae3d
ablos <34944115+ablos@users.noreply.github.com>
2018-03-14 19:43:54 +0100

    Did some work on the php side of streaming

---
+++ StreamingServer/stream.php
@@ -0,0 +1,38 @@
+<?php
+
+  // Generates a random string
+  function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+  }
+
+  // Get variables from url
+  $pass = $_GET['pass'];
+  $url = $_GET['url'];
+
+  if ($pass !== "AblosStream00") {
+    die("Wrong password!");
+  }
+
+  $id = generateRandomString(30);
+
+  while (file_exists(__DIR__ . "/streams/" . $id . ".mp3")) {
+    $id = generateRandomString(30);
+  }
+
+  include("SabreDAV/vendor/autoload.php");
+
+  $settings = array(
+    'baseUri' => "https://ablos.stackstorage.com/remote.php/webdav/",
+    'userName' => 'ablos',
+    'password' => 'AblosStack00'
+  );
+
+  $client = new Client($settings);
+
+?>
