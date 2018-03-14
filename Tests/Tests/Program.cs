439985c7ba42e93db1fda2ce30354901252c310a
ablos <34944115+ablos@users.noreply.github.com>
2018-03-14 17:36:02 +0100

    Tested WebDav Connection

--- Tests/Tests/Program.cs
+++ Tests/Tests/Program.cs
@@ -1,16 +1,39 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using WebDAVClient;
 
 namespace Tests
 {
 	class Program
 	{
+		IClient c;
+
+		#region Main
 		static void Main(string[] args)
 		{
+			Program p = new Program();
+			p.Initialize();
+			Console.ReadLine();
+		}
+		#endregion
 
+		private async void Initialize()
+		{
+			c = new Client(new NetworkCredential { UserName = "ablos", Password = "AblosStack00" });
+			c.Server = "https://ablos.stackstorage.com/";
+			c.BasePath = "/remote.php/webdav/";
+			await fetchFiles();
+		}
+
+		private async Task fetchFiles()
+		{
+			var files = await c.List();
+
+			foreach (var file in files)
+			{
+				Console.WriteLine(file.DisplayName);
+			}
 		}
 	}
 }
