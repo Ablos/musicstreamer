61c0fa3a1c6ebfbacd104529cc428da8dc307fb0
ablos <34944115+ablos@users.noreply.github.com>
2018-04-02 16:20:33 +0200

    Moved upload button

--- Windows/Windows/Form1.Designer.cs
+++ Windows/Windows/Form1.Designer.cs
@@ -39,7 +39,7 @@ namespace Windows
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(9, 50);
+			this.button1.Location = new System.Drawing.Point(499, 302);
 			this.button1.Margin = new System.Windows.Forms.Padding(2);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(56, 19);
